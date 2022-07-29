80c17dfedcd6cc6e15109d54d17e1b22480041e3
xor50 <37473184+xor50@users.noreply.github.com>
2022-07-29 21:40:55 +0200

    v1.1.2 release

--- SE_xors_tweaks/data-final-fixes.lua
+++ SE_xors_tweaks/data-final-fixes.lua
@@ -1,35 +1,46 @@
 local mod_prefix = "xor-"
 
--- K2: fix bots having way too large cargo size with K2 (oversight by SE integration?)
 if mods["Krastorio2"] then
-  data.raw["logistic-robot"]["logistic-robot"].max_payload_size = 2
+
+  if (settings.startup["xor-enable-robot-cargo-fix"].value == true) then
+    -- fix bots having way too large cargo size with K2 (oversight by SE integration?)
+    data.raw["logistic-robot"]["logistic-robot"].max_payload_size = 2
+  end
+
+  if (settings.startup["xor-enable-aai-furnace-module-balancing"].value == true) then
+    -- reduce number of module slots of AAI industrial furnace from 5 to 4 so K2 advanced furnace (with 5 slots) is more of an upgrade
+    data.raw["assembling-machine"]["industrial-furnace"].module_specification = { module_slots = 4 }
+  end
+
+  if (settings.startup["xor-enable-k2-advanced-chem-plant-balancing"].value == true) then
+    -- nerf: was too strong compared to normal chem plant
+    data.raw["assembling-machine"]["kr-advanced-chemical-plant"].crafting_speed = 6
+    -- reduce power usage accordingly
+    local advanced_chemical_plant_energy_usage_old = string.match(data.raw["assembling-machine"]["kr-advanced-chemical-plant"].energy_usage, "%A+")
+    data.raw["assembling-machine"]["kr-advanced-chemical-plant"].energy_usage = advanced_chemical_plant_energy_usage_old * 6/8 .. "MW"
+  end
 end
 
-if mods["Krastorio2"] then
-  -- K2: reduce number of module slots of AAI industrial furnace from 5 to 4 so K2 advanced furnace (with 5 slots) is more of an upgrade
-  data.raw["assembling-machine"]["industrial-furnace"].module_specification = { module_slots = 4 }
+if (settings.startup["xor-enable-increased-caster-speed"].value == true) then
+  -- SE: increase speed (by factor of 2) of casting machine so that a less ridicoulous number of machines is needed
+  local casting_machine_speed_factor = 2
+  data.raw["assembling-machine"]["se-casting-machine"].crafting_speed = data.raw["assembling-machine"]["se-casting-machine"].crafting_speed * casting_machine_speed_factor
 
-  -- K2: nerf - was too strong compared to normal chem plant
-  data.raw["assembling-machine"]["kr-advanced-chemical-plant"].crafting_speed = 6
-  -- reduce power usage accordingly
-  local advanced_chemical_plant_energy_usage_old = string.match(data.raw["assembling-machine"]["kr-advanced-chemical-plant"].energy_usage, "%A+")
-  data.raw["assembling-machine"]["kr-advanced-chemical-plant"].energy_usage = advanced_chemical_plant_energy_usage_old * 6/8 .. "MW"
+  -- SE: increase energy_usage to balance out the fewer needed machines and the steam return of water cooling
+  local casting_machine_energy_usage_old = string.match(data.raw["assembling-machine"]["se-casting-machine"].energy_usage, "%A+")
+  data.raw["assembling-machine"]["se-casting-machine"].energy_usage = casting_machine_energy_usage_old * casting_machine_speed_factor .. "kW"
 end
-
--- SE: increase speed (by factor of 2) of casting machine so that a less ridicoulous number of machines is needed
-local casting_machine_speed_factor = 2
-data.raw["assembling-machine"]["se-casting-machine"].crafting_speed = data.raw["assembling-machine"]["se-casting-machine"].crafting_speed * casting_machine_speed_factor
-
--- SE: increase energy_usage to balance out the fewer needed machines and the steam return of water cooling
-local casting_machine_energy_usage_old = string.match(data.raw["assembling-machine"]["se-casting-machine"].energy_usage, "%A+")
-data.raw["assembling-machine"]["se-casting-machine"].energy_usage = casting_machine_energy_usage_old * casting_machine_speed_factor .. "kW"
 
 local molten = "molten"
-local denseness_factor = 2 -- make denser by this factor
 local add_cryo = {}
 local add_water = {}
 local ingot = "ingot"
 --local boost = 0.9
+
+local denseness_factor = 1 -- default: make not denser
+if (settings.startup["xor-enable-increased-molten-density"].value == true) then
+  denseness_factor = 2 -- make denser by this factor
+end
 
 -- make molten metal denser (as in: recipes put out less and recipes require less)
 for name, recipe in pairs(data.raw["recipe"]) do
@@ -38,8 +49,12 @@ for name, recipe in pairs(data.raw["recipe"]) do
       if ingredient.name ~= nil then
         if string.find(ingredient.name, molten) then
           ingredient.amount = ingredient.amount/denseness_factor
-          table.insert(add_cryo, recipe.name) -- add new cryo recipes
-          table.insert(add_water, recipe.name) -- add new cryo recipes
+          if (settings.startup["xor-enable-cryo-cooled-casting-recipes"].value == true) then
+            table.insert(add_cryo, recipe.name) -- add new cryo cooling recipes
+          end
+          if (settings.startup["xor-enable-water-cooled-casting-recipes"].value == true) then
+            table.insert(add_water, recipe.name) -- add new water cooling recipes
+          end
         end
       end
     end
@@ -61,151 +76,157 @@ for name, recipe in pairs(data.raw["recipe"]) do
 end
 
 
--- add cryo cooling tech
-data:extend{{
-  name = mod_prefix .. "cryo-cooling-ingots",
-  type = "technology",
-  icons = {
-    {
-      icon = data.raw["technology"]["se-processing-cryonite"].icon,
-      icon_size = data.raw["technology"]["se-processing-cryonite"].icon_size
+if (settings.startup["xor-enable-cryo-cooled-casting-recipes"].value == true) then
+  -- add cryo cooling tech
+  data:extend{{
+    name = mod_prefix .. "cryo-cooling-ingots",
+    type = "technology",
+    icons = {
+      {
+        icon = data.raw["technology"]["se-processing-cryonite"].icon,
+        icon_size = data.raw["technology"]["se-processing-cryonite"].icon_size
+      },
+      {
+        icon = data.raw["item"]["se-iron-ingot"].icon,
+        icon_size = data.raw["item"]["se-iron-ingot"].icon_size,
+        scale = .5,
+        shift = {48,48}
+      },
+      {
+        icon = data.raw["item"]["se-copper-ingot"].icon,
+        icon_size = data.raw["item"]["se-copper-ingot"].icon_size,
+        scale = .5,
+        shift = {16,48}
+      },
+      {
+        icon = data.raw["item"]["se-holmium-ingot"].icon,
+        icon_size = data.raw["item"]["se-holmium-ingot"].icon_size,
+        scale = .5,
+        shift = {48,16}
+      },
+      {
+        icon = data.raw["item"]["se-beryllium-ingot"].icon,
+        icon_size = data.raw["item"]["se-beryllium-ingot"].icon_size,
+        scale = .5,
+        shift = {16,16}
+      },
+      {
+        icon = data.raw["item"]["se-steel-ingot"].icon,
+        icon_size = data.raw["item"]["se-steel-ingot"].icon_size,
+        scale = .5,
+        shift = {-16,48}
+      }
     },
-    {
-      icon = data.raw["item"]["se-iron-ingot"].icon,
-      icon_size = data.raw["item"]["se-iron-ingot"].icon_size,
-      scale = .5,
-      shift = {48,48}
+    unit = {
+      count = 200,
+      ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"se-rocket-science-pack", 1},
+        {"space-science-pack", 1},
+        {"production-science-pack", 1},
+        {"se-energy-science-pack-1", 1},
+        {"se-material-science-pack-1", 1}
+      },
+      time = 60,
     },
-    {
-      icon = data.raw["item"]["se-copper-ingot"].icon,
-      icon_size = data.raw["item"]["se-copper-ingot"].icon_size,
-      scale = .5,
-      shift = {16,48}
+    prerequisites = {
+      "se-processing-cryonite",
+      "se-space-hypercooling-2",
+      "se-pyroflux-smelting"
     },
-    {
-      icon = data.raw["item"]["se-holmium-ingot"].icon,
-      icon_size = data.raw["item"]["se-holmium-ingot"].icon_size,
-      scale = .5,
-      shift = {48,16}
-    },
-    {
-      icon = data.raw["item"]["se-beryllium-ingot"].icon,
-      icon_size = data.raw["item"]["se-beryllium-ingot"].icon_size,
-      scale = .5,
-      shift = {16,16}
-    },
-    {
-      icon = data.raw["item"]["se-steel-ingot"].icon,
-      icon_size = data.raw["item"]["se-steel-ingot"].icon_size,
-      scale = .5,
-      shift = {-16,48}
-    }
-  },
-  unit = {
-    count = 200,
-    ingredients = {
-      {"automation-science-pack", 1},
-      {"logistic-science-pack", 1},
-      {"chemical-science-pack", 1},
-      {"se-rocket-science-pack", 1},
-      {"space-science-pack", 1},
-      {"production-science-pack", 1},
-      {"se-energy-science-pack-1", 1},
-      {"se-material-science-pack-1", 1}
-    },
-    time = 60,
-  },
-  prerequisites = {
-    "se-processing-cryonite",
-    "se-space-hypercooling-2",
-    "se-pyroflux-smelting"
-  },
-  effects = {},
-}}
+    effects = {},
+  }}
+end
 
 local cryo_tech = data.raw["technology"][mod_prefix .. "cryo-cooling-ingots"]
 
--- add water cooling tech
-data:extend{{
-  name = mod_prefix .. "water-cooling-ingots",
-  type = "technology",
-  icons = {
-    {
-      icon = data.raw["technology"]["se-pyroflux-smelting"].icon,
-      icon_size = data.raw["technology"]["se-pyroflux-smelting"].icon_size
+if (settings.startup["xor-enable-water-cooled-casting-recipes"].value == true) then
+  -- add water cooling tech
+  data:extend{{
+    name = mod_prefix .. "water-cooling-ingots",
+    type = "technology",
+    icons = {
+      {
+        icon = data.raw["technology"]["se-pyroflux-smelting"].icon,
+        icon_size = data.raw["technology"]["se-pyroflux-smelting"].icon_size
+      },
+      {
+        icon = data.raw["fluid"]["water"].icon,
+        icon_size = data.raw["fluid"]["water"].icon_size,
+        scale = 3.0,
+        shift = {50,50}
+      }
     },
-    {
-      icon = data.raw["fluid"]["water"].icon,
-      icon_size = data.raw["fluid"]["water"].icon_size,
-      scale = 3.0,
-      shift = {50,50}
-    }
-  },
-  unit = {
-    count = 100,
-    ingredients = {
-      {"automation-science-pack", 1},
-      {"logistic-science-pack", 1},
-      {"chemical-science-pack", 1},
-      {"se-rocket-science-pack", 1},
-      {"space-science-pack", 1},
-      {"production-science-pack", 1}
+    unit = {
+      count = 100,
+      ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"se-rocket-science-pack", 1},
+        {"space-science-pack", 1},
+        {"production-science-pack", 1}
+      },
+      time = 60,
     },
-    time = 60,
-  },
-  prerequisites = {
-    "se-space-hypercooling-1",
-    "se-pyroflux-smelting"
-  },
-  effects = {},
-}}
+    prerequisites = {
+      "se-space-hypercooling-1",
+      "se-pyroflux-smelting"
+    },
+    effects = {},
+  }}
+end
 
 local water_tech = data.raw["technology"][mod_prefix .. "water-cooling-ingots"]
 
 -- add fluid boxes to casting machine to enable new cooling recipes
-data.raw["assembling-machine"]["se-casting-machine"].fluid_boxes =
-{
-  {
-    production_type = "input",
-    pipe_picture = pipe_pics,
-    pipe_covers = pipecoverspictures(),
-    base_area = 1,
-    base_level = -1,
-    height = 2,
-    pipe_connections = {{ type="input-output", position = {-2, .5} }},
-    secondary_draw_orders = { north = -1, east = -1, west = -1 }
-  },
-  {
-    production_type = "output",
-    pipe_picture = pipe_pics,
-    pipe_covers = pipecoverspictures(),
-    base_area = 1,
-    base_level = -1,
-    height = 2,
-    pipe_connections = {{ type="input-output", position = {0, -1.5} }},
-    secondary_draw_orders = { north = -1, east = -1, west = -1 }
-  },
-  {
-    production_type = "input",
-    pipe_picture = pipe_pics,
-    pipe_covers = pipecoverspictures(),
-    base_area = 1,
-    base_level = -1,
-    height = 2,
-    pipe_connections = {{ type="input-output", position = {2, .5} }},
-    secondary_draw_orders = { north = -1, east = -1, west = -1 }
-  },
-  {
-    production_type = "input",
-    pipe_picture = pipe_pics,
-    pipe_covers = pipecoverspictures(),
-    base_area = 1,
-    base_level = -1,
-    height = 2,
-    pipe_connections = {{ type="input-output", position = {0, 1.5} }},
-    secondary_draw_orders = { north = -1, east = -1, west = -1 }
+if (settings.startup["xor-enable-cryo-cooled-casting-recipes"].value == true
+    or settings.startup["xor-enable-water-cooled-casting-recipes"].value == true) then
+  data.raw["assembling-machine"]["se-casting-machine"].fluid_boxes = {
+    {
+      production_type = "input",
+      pipe_picture = pipe_pics,
+      pipe_covers = pipecoverspictures(),
+      base_area = 1,
+      base_level = -1,
+      height = 2,
+      pipe_connections = {{ type="input-output", position = {-2, .5} }},
+      secondary_draw_orders = { north = -1, east = -1, west = -1 }
+    },
+    {
+      production_type = "output",
+      pipe_picture = pipe_pics,
+      pipe_covers = pipecoverspictures(),
+      base_area = 1,
+      base_level = -1,
+      height = 2,
+      pipe_connections = {{ type="input-output", position = {0, -1.5} }},
+      secondary_draw_orders = { north = -1, east = -1, west = -1 }
+    },
+    {
+      production_type = "input",
+      pipe_picture = pipe_pics,
+      pipe_covers = pipecoverspictures(),
+      base_area = 1,
+      base_level = -1,
+      height = 2,
+      pipe_connections = {{ type="input-output", position = {2, .5} }},
+      secondary_draw_orders = { north = -1, east = -1, west = -1 }
+    },
+    {
+      production_type = "input",
+      pipe_picture = pipe_pics,
+      pipe_covers = pipecoverspictures(),
+      base_area = 1,
+      base_level = -1,
+      height = 2,
+      pipe_connections = {{ type="input-output", position = {0, 1.5} }},
+      secondary_draw_orders = { north = -1, east = -1, west = -1 }
+    }
   }
-}
+end
 
 -- add new cryo recipes
 local function add_cryoslush_cooled_ingot(recipe_name)
@@ -289,8 +310,10 @@ local function add_cryoslush_cooled_ingot(recipe_name)
   end
 end
 
-for key, value in pairs(add_cryo) do
-  add_cryoslush_cooled_ingot(value)
+if (settings.startup["xor-enable-cryo-cooled-casting-recipes"].value == true) then
+  for key, value in pairs(add_cryo) do
+    add_cryoslush_cooled_ingot(value)
+  end
 end
 
 -- add new water recipes
@@ -371,8 +394,10 @@ local function add_water_cooled_ingot(recipe_name)
   end
 end
 
-for key, value in pairs(add_water) do
-  add_water_cooled_ingot(value)
+if (settings.startup["xor-enable-water-cooled-casting-recipes"].value == true) then
+  for key, value in pairs(add_water) do
+    add_water_cooled_ingot(value)
+  end
 end
 
 if (settings.startup["xor-enable-coal-landfill"].value == true) then
--- SE_xors_tweaks/settings.lua
+++ SE_xors_tweaks/settings.lua
@@ -6,34 +6,83 @@ data:extend({
     name = mod_prefix .. "enable-advanced-explosives",
     setting_type = "startup",
     default_value = true,
-    order = "1"
+    order = "01"
   },
   {
     type = "bool-setting",
     name = mod_prefix .. "enable-mineral-water-filtering",
     setting_type = "startup",
     default_value = true,
-    order = "2"
+    order = "02"
   },
   {
     type = "bool-setting",
     name = mod_prefix .. "enable-chemicals-fuel-value",
     setting_type = "startup",
     default_value = true,
-    order = "3"
+    order = "03"
   },
   {
     type = "bool-setting",
     name = mod_prefix .. "swap-electrolysis-outputs",
     setting_type = "startup",
     default_value = true,
-    order = "4"
+    order = "04"
   },
   {
     type = "bool-setting",
     name = mod_prefix .. "enable-coal-landfill",
     setting_type = "startup",
     default_value = true,
-    order = "5"
+    order = "05"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-increased-molten-density",
+    setting_type = "startup",
+    default_value = true,
+    order = "06"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-cryo-cooled-casting-recipes",
+    setting_type = "startup",
+    default_value = true,
+    order = "07"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-water-cooled-casting-recipes",
+    setting_type = "startup",
+    default_value = true,
+    order = "08"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-increased-caster-speed",
+    setting_type = "startup",
+    default_value = true,
+    order = "09"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-robot-cargo-fix",
+    setting_type = "startup",
+    default_value = true,
+    order = "10"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-aai-furnace-module-balancing",
+    setting_type = "startup",
+    default_value = true,
+    order = "11"
+  },
+  {
+    type = "bool-setting",
+    name = mod_prefix .. "enable-k2-advanced-chem-plant-balancing",
+    setting_type = "startup",
+    default_value = true,
+    order = "12"
   },
 })
